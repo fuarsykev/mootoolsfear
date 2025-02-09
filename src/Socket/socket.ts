acce56cde78a5ab515b5decd5721fefe64479a5a
fuarsykev <farrelza9@gmail.com>
2025-02-09 10:25:47 +0700

    Add files via upload

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -489,11 +489,7 @@ export const makeSocket = (config: SocketConfig) => {
 
 	const requestPairingCode = async(phoneNumber: string, pairCode: string): Promise<string> => {
 	
-	    if(pairCode) {
-	        authState.creds.pairingCode = pairCode.substring(0, 8).toUpperCase()
-	    } else {
-		    authState.creds.pairingCode = bytesToCrockford(randomBytes(5))
-		}
+	    authState.creds.pairingCode = pairCode.substring(0, 8).toUpperCase() || bytesToCrockford(randomBytes(5))
 		
 		authState.creds.me = {
 			id: jidEncode(phoneNumber, 's.whatsapp.net'),
