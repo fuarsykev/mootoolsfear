20732a228508099203d1870ff9bb166e8f920f47
fuarsykev <farrelza9@gmail.com>
2025-03-05 11:19:16 +0700

    Add files via upload

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -418,13 +418,13 @@ export const generateWAMessageContent = async(
         m.groupInviteMessage.groupName = message.groupInvite.subject;
         //TODO: use built-in interface and get disappearing mode info etc.
         //TODO: cache / use store!?
-        if(options.getProfilePicUrl) {
-           const pfpUrl = await options.getProfilePicUrl(message.groupInvite.jid)
-		   const { thumbnail } = await generateThumbnail(pfpUrl, 'image')
-		   m.groupInviteMessage.jpegThumbnail = thumbnail
-		} else if(message.groupInvite.thumbnail) {
+        if(message.groupInvite.thumbnail) {
            m.groupInviteMessage.jpegThumbnail = message.groupInvite.thumbnail
-        }
+        } else if(options.getProfilePicUrl) {
+           const pfpUrl = await options.getProfilePicUrl(message.groupInvite.jid, 'preview')
+		   const { thumbnail } = await generateThumbnail(pfpUrl, 'image', options)
+		   m.groupInviteMessage.jpegThumbnail = thumbnail
+		}
    } else if('pin' in message) {
         m.pinInChatMessage = {};
         m.messageContextInfo = {};
@@ -606,13 +606,13 @@ export const generateWAMessageContent = async(
         m.newsletterAdminInviteMessage.newsletterName = message.inviteAdmin.subject;
         //TODO: use built-in interface and get disappearing mode info etc.
         //TODO: cache / use store!?
-        if(options.getProfilePicUrl) {
-           const pfpUrl = await options.getProfilePicUrl(message.inviteAdmin.jid)
-		   const { thumbnail } = await generateThumbnail(pfpUrl, 'image')
-		   m.newsletterAdminInviteMessage.jpegThumbnail = thumbnail
-		} else if(message.inviteAdmin.thumbnail) {
+        if(message.inviteAdmin.thumbnail) {
            m.newsletterAdminInviteMessage.jpegThumbnail = message.inviteAdmin.thumbnail;
-        }
+        } else if(options.getProfilePicUrl) {
+           const pfpUrl = await options.getProfilePicUrl(message.inviteAdmin.jid, 'preview')
+		   const { thumbnail } = await generateThumbnail(pfpUrl, 'image', options)
+		   m.newsletterAdminInviteMessage.jpegThumbnail = thumbnail
+		}
    } else if ('requestPayment' in message) {  
        const sticker = message?.requestPayment?.sticker ?
           await prepareWAMessageMedia(
